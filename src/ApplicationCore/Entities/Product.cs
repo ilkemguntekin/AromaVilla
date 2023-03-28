23e463f98aecc3e8fb18a2dc711d7d29ca75bb0c
ilkemguntekin <ilkemguntekin@gmail.com>
2023-03-28 11:11:19 +0300

    Entity classes have been created.

---
+++ src/ApplicationCore/Entities/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? PictureUri { get; set; }
+        public int CategoryId { get; set; }
+        public int BrandId { get; set; }
+        public Category Category { get; set; }
+        public Brand Brand { get; set; }
+    }
+}
