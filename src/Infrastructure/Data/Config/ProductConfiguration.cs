f57d2eebd5c1a5c2764a9aaa278b07c9c3b004e1
ilkemguntekin <ilkemguntekin@gmail.com>
2023-12-04 20:15:46 +0300

    entities, interface, dbcontext.

---
+++ src/Infrastructure/Data/Config/ProductConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data.Config
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name)
+                    .HasMaxLength(100);
+
+            builder.Property(x => x.Price)
+                    .HasPrecision(18, 2);
+        }
+    }
+}
